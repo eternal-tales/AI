0c251de5761523f4a89f297d6f805cdab5b2c849
dhdbsrlw <dhdbsrlw@korea.ac.kr>
2024-02-02 14:38:31 +0900

    Feat: config, model, dreambooth (need to set path)

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ai
+
+순서: config(dependency) - model download - dreambooth - instance images(생략 가능) - training - inference (generate)
--- fastapi/dreambooth.py
+++ fastapi/dreambooth.py
@@ -1,36 +1,42 @@
 import os
-from IPython.display import clear_output
-from IPython.utils import capture
-from os import listdir
-from os.path import isfile
-from subprocess import check_output
-import wget
+import shutil
+import subprocess
 import time
+import wget
+from subprocess import check_output
 
-#@markdown #Create/Load a Session
+# Create/Load a Session
 
+Session_Name = ""  # param{type: 'string'} - 수정 필요
+MODEL_NAME = ""  # param{type: 'string'} - 수정 필요
+
+WORKSPACE = '/content/Fast-Dreambooth'
+SESSION_DIR = f"{WORKSPACE}/Sessions/{Session_Name}"
+
+'''
 try:
   MODEL_NAME
   pass
 except:
   MODEL_NAME=""
+'''
 
-PT=""
+PT = ""
 
-Session_Name = "try01" #@param{type: 'string'}
-while Session_Name=="":
-  print('[1;31mInput the Session Name:')
-  Session_Name=input('')
-Session_Name=Session_Name.replace(" ","_")
+while Session_Name == "":
+    print('[1;31mInput the Session Name:')
+    Session_Name = input('')
+Session_Name = Session_Name.replace(" ", "_")
 
-#@markdown - Enter the session name, it if it exists, it will load it, otherwise it'll create an new session.
+# @markdown - Enter the session name, it if it exists, it will load it, otherwise it'll create an new session.
 
-Session_Link_optional = "" #@param{type: 'string'}
+Session_Link_optional = ""  # @param{type: 'string'}
 
-#@markdown - Import a session from another gdrive, the shared gdrive link must point to the specific session's folder that contains the trained CKPT, remove any intermediary CKPT if any.
+# @markdown - Import a session from another gdrive, the shared gdrive link must point to the specific session's folder that contains the trained CKPT, remove any intermediary CKPT if any.
 
-WORKSPACE='/content/gdrive/MyDrive/Fast-Dreambooth'
+WORKSPACE = '/content/Fast-Dreambooth'
 
+'''
 if Session_Link_optional !="":
   print('[1;32mDownloading session...')
   with capture.capture_output() as cap:
@@ -46,99 +52,110 @@ if Session_Link_optional !="":
     !rm -r captions
     !unzip captions.zip
     %cd /content
+'''
 
 
-INSTANCE_NAME=Session_Name
-OUTPUT_DIR="/content/models/"+Session_Name
-SESSION_DIR=WORKSPACE+'/Sessions/'+Session_Name
-INSTANCE_DIR=SESSION_DIR+'/instance_images'
-CAPTIONS_DIR=SESSION_DIR+'/captions'
-MDLPTH=str(SESSION_DIR+"/"+Session_Name+'.ckpt')
+INSTANCE_NAME = Session_Name
+OUTPUT_DIR = "/content/models/"+Session_Name
+SESSION_DIR = WORKSPACE+'/Sessions/'+Session_Name
+INSTANCE_DIR = SESSION_DIR+'/instance_images'
+CAPTIONS_DIR = SESSION_DIR+'/captions'
+MDLPTH = str(SESSION_DIR+"/"+Session_Name+'.ckpt')
 
 if os.path.exists(str(SESSION_DIR)):
-  mdls=[ckpt for ckpt in listdir(SESSION_DIR) if ckpt.split(".")[-1]=="ckpt"]
-  if not os.path.exists(MDLPTH) and '.ckpt' in str(mdls):
+    mdls = [ckpt for ckpt in listdir(
+        SESSION_DIR) if ckpt.split(".")[-1] == "ckpt"]
+    if not os.path.exists(MDLPTH) and '.ckpt' in str(mdls):
 
-    def f(n):
-      k=0
-      for i in mdls:
-        if k==n:
-          !mv "$SESSION_DIR/$i" $MDLPTH
-        k=k+1
+        def f(n):
+            k = 0
+            for i in mdls:
+                if k == n:
+                    # !mv "$SESSION_DIR/$i" $MDLPTH
+                    source_path = os.path.join(SESSION_DIR, i)
+                    shutil.move(source_path, MDLPTH)
+                k = k+1
 
-    k=0
-    print('[1;33mNo final checkpoint model found, select which intermediary checkpoint to use, enter only the number, (000 to skip):\n[1;34m')
+        k = 0
+        print('[1;33mNo final checkpoint model found, select which intermediary checkpoint to use, enter only the number, (000 to skip):\n[1;34m')
 
-    for i in mdls:
-      print(str(k)+'- '+i)
-      k=k+1
-    n=input()
-    while int(n)>k-1:
-      n=input()
-    if n!="000":
-      f(int(n))
-      print('[1;32mUsing the model '+ mdls[int(n)]+" ...")
-      time.sleep(2)
-    else:
-      print('[1;32mSkipping the intermediary checkpoints.')
-    del n
+        for i in mdls:
+            print(str(k)+'- '+i)
+            k = k+1
+        n = input()
+        while int(n) > k-1:
+            n = input()
+        if n != "000":
+            f(int(n))
+            print('[1;32mUsing the model ' + mdls[int(n)]+" ...")
+            time.sleep(2)
+        else:
+            print('[1;32mSkipping the intermediary checkpoints.')
+        del n
 
+'''
 with capture.capture_output() as cap:
   %cd /content
   resume=False
+'''
 
 if os.path.exists(str(SESSION_DIR)) and not os.path.exists(MDLPTH):
-  print('[1;32mLoading session with no previous model, using the original model or the custom downloaded model')
-  if MODEL_NAME=="":
-    print('[1;31mNo model found, use the "Model Download" cell to download a model.')
-  else:
-    print('[1;32mSession Loaded, proceed to uploading instance images')
+    print('[1;32mLoading session with no previous model, using the original model or the custom downloaded model')
+    if MODEL_NAME == "":
+        print(
+            '[1;31mNo model found, use the "Model Download" cell to download a model.')
+    else:
+        print('[1;32mSession Loaded, proceed to uploading instance images')
 
 elif os.path.exists(MDLPTH):
-  print('[1;32mSession found, loading the trained model ...')
-  wget.download('https://github.com/TheLastBen/fast-stable-diffusion/raw/main/Dreambooth/det.py')
-  print('[1;33mDetecting model version...')
-  Model_Version=check_output('python det.py --MODEL_PATH '+MDLPTH, shell=True).decode('utf-8').replace('\n', '')
-  clear_output()
-  print('[1;32m'+Model_Version+' Detected')
-  !rm det.py
-  if Model_Version=='1.5':
-    !wget -q -O config.yaml https://github.com/CompVis/stable-diffusion/raw/main/configs/stable-diffusion/v1-inference.yaml
-    print('[1;32mSession found, loading the trained model ...')
-    !python /content/diffusers/scripts/convert_original_stable_diffusion_to_diffusers.py --checkpoint_path $MDLPTH --dump_path "$OUTPUT_DIR" --original_config_file config.yaml
-    !rm /content/config.yaml
 
-  elif Model_Version=='V2.1-512px':
-    !wget -q -O convertodiff.py https://raw.githubusercontent.com/TheLastBen/fast-stable-diffusion/main/Dreambooth/convertodiffv2.py
-    print('[1;32mSession found, loading the trained model ...')
-    !python /content/convertodiff.py "$MDLPTH" "$OUTPUT_DIR" --v2 --reference_model stabilityai/stable-diffusion-2-1-base
-    !rm /content/convertodiff.py
+    print('\033[1;32mSession found, loading the trained model ...')
+    # Download and run the model version detection script
+    det_script_url = 'https://github.com/TheLastBen/fast-stable-diffusion/raw/main/Dreambooth/det.py'
+    det_script_path = wget.download(det_script_url)
+    print('\033[1;33mDetecting model version...')
+    Model_Version = subprocess.check_output(
+        ['python', det_script_path, '--MODEL_PATH', MDLPTH], text=True).strip()
+    os.remove(det_script_path)  # Remove the det.py script after use
+    print('\033[1;32m' + Model_Version + ' Detected')
 
-  elif Model_Version=='V2.1-768px':
-    !wget -q -O convertodiff.py https://github.com/TheLastBen/fast-stable-diffusion/raw/main/Dreambooth/convertodiffv2-768.py
-    print('[1;32mSession found, loading the trained model ...')
-    !python /content/convertodiff.py "$MDLPTH" "$OUTPUT_DIR" --v2 --reference_model stabilityai/stable-diffusion-2-1
-    !rm /content/convertodiff.py
+    if Model_Version == '1.5':
+        config_yaml_url = 'https://github.com/CompVis/stable-diffusion/raw/main/configs/stable-diffusion/v1-inference.yaml'
+        config_yaml_path = wget.download(config_yaml_url)
+        print('\033[1;32mSession found, loading the trained model ...')
+        subprocess.run(['python', '/content/diffusers/scripts/convert_original_stable_diffusion_to_diffusers.py',
+                        '--checkpoint_path', MDLPTH, '--dump_path', OUTPUT_DIR, '--original_config_file', config_yaml_path])
+        os.remove(config_yaml_path)  # Remove the config file after use
 
+    elif Model_Version == 'V2.1-512px':
+        convert_script_url = 'https://raw.githubusercontent.com/TheLastBen/fast-stable-diffusion/main/Dreambooth/convertodiffv2.py'
+        convert_script_path = wget.download(convert_script_url)
+        print('\033[1;32mSession found, loading the trained model ...')
+        subprocess.run(['python', convert_script_path, MDLPTH, OUTPUT_DIR,
+                       '--v2', '--reference_model', 'stabilityai/stable-diffusion-2-1-base'])
+        os.remove(convert_script_path)  # Remove the convert script after use
 
-  if os.path.exists(OUTPUT_DIR+'/unet/diffusion_pytorch_model.bin'):
-    resume=True
-    clear_output()
-    print('[1;32mSession loaded.')
-  else:
-    if not os.path.exists(OUTPUT_DIR+'/unet/diffusion_pytorch_model.bin'):
-      print('[1;31mConversion error, if the error persists, remove the CKPT file from the current session folder')
+    elif Model_Version == 'V2.1-768px':
+        convert_script_url = 'https://github.com/TheLastBen/fast-stable-diffusion/raw/main/Dreambooth/convertodiffv2-768.py'
+        convert_script_path = wget.download(convert_script_url)
+        print('\033[1;32mSession found, loading the trained model ...')
+        subprocess.run(['python', convert_script_path, MDLPTH, OUTPUT_DIR,
+                       '--v2', '--reference_model', 'stabilityai/stable-diffusion-2-1'])
+        os.remove(convert_script_path)  # Remove the convert script after use
+
+    if os.path.exists(OUTPUT_DIR+'/unet/diffusion_pytorch_model.bin'):
+        resume = True
+        print('[1;32mSession loaded.')
+    else:
+        if not os.path.exists(OUTPUT_DIR+'/unet/diffusion_pytorch_model.bin'):
+            print(
+                '[1;31mConversion error, if the error persists, remove the CKPT file from the current session folder')
 
 elif not os.path.exists(str(SESSION_DIR)):
-    %mkdir -p "$INSTANCE_DIR"
+    os.makedirs(INSTANCE_DIR, exist_ok=True)
     print('[1;32mCreating session...')
-    if MODEL_NAME=="":
-      print('[1;31mNo model found, use the "Model Download" cell to download a model.')
+    if MODEL_NAME == "":
+        print(
+            '[1;31mNo model found, use the "Model Download" cell to download a model.')
     else:
-      print('[1;32mSession created, proceed to uploading instance images')
-
-    #@markdown
-
-    #@markdown # The most important step is to rename the instance pictures of each subject to a unique unknown identifier, example :
-    #@markdown - If you have 10 pictures of yourself, simply select them all and rename only one to the chosen identifier for example : phtmejhn, the files would be : phtmejhn (1).jpg, phtmejhn (2).png ....etc then upload them, do the same for other people or objects with a different identifier, and that's it.
-    #@markdown - Checkout this example : https://i.imgur.com/d2lD3rz.jpeg
+        print('[1;32mSession created, proceed to uploading instance images')
